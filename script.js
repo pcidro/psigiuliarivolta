e01f405aad58b309d096532d48d98768e363b49c
Paulo Cidro <paulocidro@gmail.com>
2025-11-30 12:15:59 -0300

    testimonials section and howihelp

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
-// Menu hamburguer
 import { initMenu } from "./modules/hamburguer.js";
+import SlideNav from "./modules/depoimentos.js";
+
+const slide = new SlideNav(".slide", ".slide-wrapper");
+slide.init();
+slide.addArrow(".prev", ".next");
+slide.addControl();
 initMenu();
