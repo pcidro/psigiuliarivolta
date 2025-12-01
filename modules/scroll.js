86e977be8e1142f2b3de2d5c1b6afcd6fa00ce4c
Paulo Cidro <paulocidro@gmail.com>
2025-12-01 13:49:34 -0300

    fadein

--- modules/scroll.js
+++ modules/scroll.js
@@ -1,13 +1,18 @@
 const sections = document.querySelectorAll(".js-scroll");
 
 export function animaScroll() {
-  const windowMetade = window.innerHeight * 0.6;
+  const windowMetade = window.innerHeight * 0.5;
 
   sections.forEach((section) => {
     const sectionTop = section.getBoundingClientRect().top;
 
     if (sectionTop < windowMetade) {
       section.classList.add("ativo");
+
+      const anim = section.dataset.anim;
+      if (anim) {
+        section.classList.add("animate__animated", anim);
+      }
     }
   });
 }
